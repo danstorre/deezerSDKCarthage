58de6a2227229c03eccc2413418466d2616c771b
Daniel Torres <dtorres@eudaimonia.com.ar>
2018-02-09 16:56:03 -0300

    first commit

---
+++ deezer/deezer.h
@@ -0,0 +1,19 @@
+//
+//  deezer.h
+//  deezer
+//
+//  Created by Daniel Torres on 2/9/18.
+//  Copyright © 2018 Eudaimonia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for deezer.
+FOUNDATION_EXPORT double deezerVersionNumber;
+
+//! Project version string for deezer.
+FOUNDATION_EXPORT const unsigned char deezerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <deezer/PublicHeader.h>
+
+
---
+++ deezerSDK/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  deezerSDK
+//
+//  Created by Daniel Torres on 2/9/18.
+//  Copyright © 2018 Eudaimonia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
